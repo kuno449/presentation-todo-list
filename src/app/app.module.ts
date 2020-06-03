62fcb669ae73dfec63a2948eb88db42bc845ff1d
yk_epikur <yoshiki.kuno@epikur.de>
2020-06-03 22:25:28 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NgbDatepickerModule} from '@ng-bootstrap/ng-bootstrap';
+
+import de from '@angular/common/locales/de';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(de, 'de');
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbDatepickerModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'de'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
